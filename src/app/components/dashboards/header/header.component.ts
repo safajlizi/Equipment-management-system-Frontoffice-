65a5a6acd294442a369273e4b62bbdb75a1d35a5
HediKhemiri3001 <khemirimedhedi@gmail.com>
2022-08-04 17:21:01 +0100

    Login working with restricted access to dashboard.

--- src/app/components/dashboards/header/header.component.ts
+++ src/app/components/dashboards/header/header.component.ts
@@ -1,25 +1,28 @@
-import { Component,EventEmitter, OnInit,Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() sideNavToggled = new EventEmitter<boolean>();
+  menuStatus: boolean = false;
+  constructor(
+    private router: Router,
+    private tokenStorage: TokenStorageService
+  ) {}
 
-  @Output() sideNavToggled= new EventEmitter<boolean>()
-  menuStatus:boolean=false 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void {
-  }
-  SideNavToggle(){
-    this.menuStatus= !this.menuStatus
-    this.sideNavToggled.emit(this.menuStatus)
+  ngOnInit(): void {}
+  SideNavToggle() {
+    this.menuStatus = !this.menuStatus;
+    this.sideNavToggled.emit(this.menuStatus);
   }
   ingresar() {
-    this.router.navigateByUrl('/login');
+    this.tokenStorage.signOut();
+    this.router.navigateByUrl('/');
   }
-
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+const AUTH_API = 'http://localhost:3000/auth/';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+  login(identifier: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'login',
+      {
+        identifier,
+        password,
+      },
+      httpOptions
+    );
+  }
+  register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'register',
+      {
+        username,
+        email,
+        password,
+      },
+      httpOptions
+    );
+  }
+}
