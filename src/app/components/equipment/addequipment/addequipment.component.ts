63f4c3f40e8145c5f23dda78b1a877921e99aa7c
HediKhemiri3001 <khemirimedhedi@gmail.com>
2022-09-05 11:40:52 +0100

    misc.

--- src/app/components/equipment/addequipment/addequipment.component.ts
+++ src/app/components/equipment/addequipment/addequipment.component.ts
@@ -1,121 +1,139 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import{FormGroup,FormBuilder,Validators} from'@angular/forms'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EquipmentService } from 'src/app/services/equipment.service';
-import {MatDialogRef,MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { DatePipe } from '@angular/common'
-import {MatSnackBar} from '@angular/material/snack-bar';
-import {Router,ActivatedRoute} from '@angular/router'
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DatePipe } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-addequipment',
   templateUrl: './addequipment.component.html',
-  styleUrls: ['./addequipment.component.css']
+  styleUrls: ['./addequipment.component.css'],
 })
 export class AddequipmentComponent implements OnInit {
-  equipmentForm!:FormGroup;
-  actionBtn:string="save"
-  constructor(public datepipe: DatePipe,private formBuilder :FormBuilder,private api:EquipmentService,
-    @Inject(MAT_DIALOG_DATA)public editData:any, 
-    private dialogRef:MatDialogRef<AddequipmentComponent>,private _snackBar: MatSnackBar,private router:Router,private route:ActivatedRoute) { }
+  equipmentForm!: FormGroup;
+  actionBtn: string = 'save';
+  constructor(
+    public datepipe: DatePipe,
+    private formBuilder: FormBuilder,
+    private api: EquipmentService,
+    @Inject(MAT_DIALOG_DATA) public editData: any,
+    private dialogRef: MatDialogRef<AddequipmentComponent>,
+    private _snackBar: MatSnackBar,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit(): void {
-    this.equipmentForm=this.formBuilder.group({
-      label:['', Validators.required],
+    this.equipmentForm = this.formBuilder.group({
+      label: ['', Validators.required],
 
-      property:[false],
-      conformity:['compliant', Validators.required],
-      defaults:[null],
-      description:[''],
-      maker:[''],
-      serial_number:[],
-      calibration:['na'],
-      validity_date:[null, Validators.prototype],
-      category:[null, Validators.required],
-      other:[null],
-      manager:[]
-     
-    })
-    if(this.editData){
-      this.actionBtn="Update"
-      
-      
-
-
+      property: [false],
+      conformity: ['compliant', Validators.required],
+      defaults: [null],
+      description: [''],
+      maker: [''],
+      serial_number: [],
+      calibration: ['na'],
+      validity_date: [null, Validators.prototype],
+      category: [null, Validators.required],
+      other: [null],
+    });
+    if (this.editData) {
+      this.actionBtn = 'Update';
 
       this.equipmentForm.controls['property'].setValue(this.editData.property);
-      this.equipmentForm.controls['conformity'].setValue(this.editData.conformity);
+      this.equipmentForm.controls['conformity'].setValue(
+        this.editData.conformity
+      );
       this.equipmentForm.controls['defaults'].setValue(this.editData.defaults);
       this.equipmentForm.controls['other'].setValue(this.editData.other);
 
-      this.equipmentForm.controls['calibration'].setValue(this.editData.calibration);
-      this.equipmentForm.controls['validity_date'].setValue(this.editData.validity_date);
+      this.equipmentForm.controls['calibration'].setValue(
+        this.editData.calibration
+      );
+      this.equipmentForm.controls['validity_date'].setValue(
+        this.editData.validity_date
+      );
       this.equipmentForm.controls['category'].setValue(this.editData.category);
-
     }
-
-
   }
-  
-  updateEquipment(){
-    if(this.equipmentForm.value.property == true ){this.equipmentForm.controls['property'].setValue('client')}
-    else{this.equipmentForm.controls['property'].setValue('sofia')}
-    this.api.patchEquipment(this.equipmentForm.value,this.editData.id)
-    .subscribe({
-      next:(res)=>{
-        this._snackBar.open("Equipment updated successfuly",'',{ 
-          duration: 3000
-      })
-        this.equipmentForm.reset();
-        this.dialogRef.close();
-        this.router.routeReuseStrategy.shouldReuseRoute=()=>false;
-        this.router.navigate(['./'],{
-          relativeTo: this.route
-        })
-      },
-      error:()=>{
-        this._snackBar.open("error while updating equipment",'',{ 
-          duration: 3000
-      })
-      }})
 
-
-  }
-  addEquipment(){
-    if(!this.editData){
-       if(this.equipmentForm.valid){
-        if(this.equipmentForm.value.category == 'calibration' && this.equipmentForm.value.calibration =='na' ){this.equipmentForm.controls['calibration'].setValue('nok')}
-
-
-        this.equipmentForm.controls['validity_date'].setValue(this.datepipe.transform(this.equipmentForm.value.validity_date, 'yyyy-MM-dd') )
-        if(this.equipmentForm.value.property == true ){this.equipmentForm.controls['property'].setValue('client')}
-        else{this.equipmentForm.controls['property'].setValue('sofia')}
-
-        if(this.equipmentForm.value.other ){this.equipmentForm.controls['category'].setValue(this.equipmentForm.value.other)}
-      this.api.postEquipment(this.equipmentForm.value)
+  updateEquipment() {
+    if (this.equipmentForm.value.property == true) {
+      this.equipmentForm.controls['property'].setValue('client');
+    } else {
+      this.equipmentForm.controls['property'].setValue('sofia');
+    }
+    this.api
+      .patchEquipment(this.equipmentForm.value, this.editData.id)
       .subscribe({
-        next:(res)=>{
-          this._snackBar.open("Equipment added successfuly",'',{ 
-            duration: 3000
-        })
+        next: (res) => {
+          this._snackBar.open('Equipment updated successfuly', '', {
+            duration: 3000,
+          });
           this.equipmentForm.reset();
           this.dialogRef.close();
-          this.router.routeReuseStrategy.shouldReuseRoute=()=>false;
-          this.router.navigate(['./'],{
-            relativeTo: this.route
-          })
+          this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+          this.router.navigate(['./'], {
+            relativeTo: this.route,
+          });
         },
-        error:()=>{
-          this._snackBar.open("error while adding equipment",'',{ 
-            duration: 3000
-        })
+        error: () => {
+          this._snackBar.open('error while updating equipment', '', {
+            duration: 3000,
+          });
+        },
+      });
+  }
+  addEquipment() {
+    if (!this.editData) {
+      if (this.equipmentForm.valid) {
+        if (
+          this.equipmentForm.value.category == 'calibration' &&
+          this.equipmentForm.value.calibration == 'na'
+        ) {
+          this.equipmentForm.controls['calibration'].setValue('nok');
         }
-      })
-     } }
-     else{
-      this.updateEquipment()
-     }
 
-  } 
+        this.equipmentForm.controls['validity_date'].setValue(
+          this.datepipe.transform(
+            this.equipmentForm.value.validity_date,
+            'yyyy-MM-dd'
+          )
+        );
+        if (this.equipmentForm.value.property == true) {
+          this.equipmentForm.controls['property'].setValue('client');
+        } else {
+          this.equipmentForm.controls['property'].setValue('sofia');
+        }
 
-
+        if (this.equipmentForm.value.other) {
+          this.equipmentForm.controls['category'].setValue(
+            this.equipmentForm.value.other
+          );
+        }
+        this.api.postEquipment(this.equipmentForm.value).subscribe({
+          next: (res) => {
+            this._snackBar.open('Equipment added successfuly', '', {
+              duration: 3000,
+            });
+            this.equipmentForm.reset();
+            this.dialogRef.close();
+            this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+            this.router.navigate(['./'], {
+              relativeTo: this.route,
+            });
+          },
+          error: () => {
+            this._snackBar.open('error while adding equipment', '', {
+              duration: 3000,
+            });
+          },
+        });
+      }
+    } else {
+      this.updateEquipment();
+    }
+  }
 }
- 
